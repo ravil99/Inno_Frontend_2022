0786e448de975f927a2ef216dae06be9f04a4d1f
ravil99 <akhm.ravil99@gmail.com>
2022-10-10 20:48:28 +0300

    Github Actions config has been added

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,19 @@ import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
+
+const dev = process.env.NODE_ENV === 'development';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/your-repo-name',
+		  },
+		},
 };
 
 export default config;
