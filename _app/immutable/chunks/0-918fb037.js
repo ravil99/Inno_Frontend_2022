889d488d6a166ab3f04e543fd8b31093886ef961
ravil99 <48554221+ravil99@users.noreply.github.com>
2022-10-12 13:04:08 +0000

    Deploying to gh-pages from @ ravil99/Svelte_kit_homework@e6ce8341738246ff5c1c383c515a073e584daa23 🚀

--- _app/immutable/chunks/0-918fb037.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-7071eb47.js";export{t as component};
--- _app/immutable/chunks/1-a06762cc.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-4755ea69.js";export{t as component};
